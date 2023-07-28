d2cc78c4107d38d3db20192b074254a1131f6645
ayumisalt <ayumikasagi@gmail.com>
2023-07-28 09:53:36 +0000

    format_val_images

---
+++ src/format_val_images.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import os
+import shutil
+
+df = pd.read_csv("../tiny-imagenet-200/val/val_annotations.txt", sep='\t', header=None)
+
+for index, data in df.iterrows():
+    print("../tiny-imagenet-200/val/" + data[1])
+    os.makedirs("../tiny-imagenet-200/val/" + data[1], exist_ok=True)
+    print("../tiny-imagenet-200/val/" + data[1] + "/" + data[0])
+    shutil.copy("../tiny-imagenet-200/val/" + "images/" + data[0], "../tiny-imagenet-200/val/" + data[1] + "/" + data[0])
